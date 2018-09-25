465141c9a1109f825d30f525c388017d2d728159
taybur43 <sagor.ece@gmail.com>
2018-09-25 14:56:15 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python_zabbix_import
---
+++ import.py
@@ -0,0 +1,20 @@
+from pyzabbix  import ZabbixAPI
+import argparse
+parser=argparse.ArgumentParser(description="Import throgh here")
+parser.add_argument('loc',help='Please provide the valid location during run.No location validation has been implemented')
+args=parser.parse_args()
+zapi = ZabbixAPI(url='http://192.168.56.101/zabbix/', user='Admin', password='zabbix')
+print("Yes Connected")
+file=open(args.loc,"r")
+cont=file.read()
+results=zapi.do_request('configuration.import',{"format":"xml","rules":{
+"templates":{"createMissing":True,"updateExisting":True},
+"applications":{"createMissing": True},
+"items":{"createMissing": True,"updateExisting": True},
+"triggers":{"createMissing": True,"updateExisting": True},
+"graphs":{"createMissing": True,"updateExisting": True},
+},
+"source":cont})
+results["result"]=='True' 
+print("successfully imported",results)
+
