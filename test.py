63cad7058afdc69cff720b5edc880f10e01e0de6
taybur43 <sagor.ece@gmail.com>
2018-10-02 10:33:48 +0600

    this is remembering

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 print("Hi this is for testing")
 print("New Line has been added dear sir")	
+
+
+
+https://taybur.rahaman%40valmetpartners.com@alm.valmet.com/stash/scm/plmcom/zabbix.git
+https://taybur.rahaman%40valmetpartners.com@alm.valmet.com/stash/scm/plmcom/zabbix.git
