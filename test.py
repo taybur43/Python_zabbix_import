688fa56f9ae0400f48def290bd5ac98368bbc539
Md.Taybur Rahaman <43567872+taybur43@users.noreply.github.com>
2022-09-24 21:26:59 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
 print("Hi this is for testing")
 print("New Line has been added dear sir")	
 
-
-
-https://taybur.rahaman%40valmetpartners.com@alm.valmet.com/stash/scm/plmcom/zabbix.git
-https://taybur.rahaman%40valmetpartners.com@alm.valmet.com/stash/scm/plmcom/zabbix.git
